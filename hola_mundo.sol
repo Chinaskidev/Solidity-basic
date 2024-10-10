08b655d29afe7c16c0b49ec357a6220b5fba7e86
Chinaskidev <chinaskicharles0@gmail.com>
2024-10-10 10:39:06 -0300

    primeros cambios

---
+++ hola_mundo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+
+// AQUI COMIENZA A FUNCIONAR EL CEREBRO
+contract hola_mundo {
+    string public saludo= "Hola desde EL SALVADOR :)"; //declarando mi variable saludo
+
+}
